de80358427659a9fd1deb74f15ec6697f70d712d
syedshaon <syedshaon99@gmail.com>
2023-05-02 14:38:48 +0600

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 // import "normalize.css";
 // Will be using reset css through scss
+
+//  TO DISABLE DONT IMPORT * AS bOOTSTRAP ALSO DON'T IMPORT BOOTSTRAP IN STYLE.SCSS FILE
 import "./style.scss";
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+// or, specify which plugins you need:
+// import { Tooltip, Toast, Popover } from 'bootstrap'
