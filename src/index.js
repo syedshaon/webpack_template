a7d649601b9c54981668b9cb81cad0a8b05c4bee
syedshaon <syedshaon99@gmail.com>
2023-05-04 11:24:33 +0600

    added Bulma CSS Starter

--- src/index.js
+++ src/index.js
@@ -9,3 +9,19 @@ import "./style.scss";
 import * as bootstrap from "bootstrap";
 // or, specify which plugins you need:
 // import { Tooltip, Toast, Popover } from 'bootstrap'
+
+/* Following is required for Bulma CSS Hamburger Menu */
+
+const el = document.querySelector(".navbar-burger");
+
+el.addEventListener("click", () => {
+  const targetExpand = document.getElementById("navbarBasicExample");
+
+  // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+  el.classList.toggle("is-active");
+  targetExpand.classList.toggle("is-active");
+});
+
+/* Bulma CSS Hamburger Menu Ends */
+
+/* npx kill-port 3000 */
